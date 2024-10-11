1e6b6e0e720e1c6b1afa0fc17e54282da0db2bca
Thales R. Menezes <thalesmenezes286@gmail.com>
2024-10-10 22:13:05 -0300

    feat: signup-component

--- login-page/src/app/app.routes.ts
+++ login-page/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
     {
-        path:"login",
+        path: "",
         component: LoginComponent
+    },
+    {
+        path: "signup",
+        component: SignUpComponent
     }
 ];
--- login-page/src/app/services/login.service.ts
+++ login-page/src/app/services/login.service.ts
@@ -20,4 +20,13 @@ export class LoginService {
       })
     )
   }
+
+  signup(name: string, email: string, password: string){
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    )
+  }
 }
